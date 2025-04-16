23dfa5871ca854cb802d69dbd8e4a3bf62935103
LiuYiBest <1070692044@qq.com>
2025-04-16 22:42:06 +0800

    mail moudle

---
+++ src/common/mail/templates/mail.module.ts
@@ -0,0 +1,25 @@
+//app.module.ts
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: 'smtps://user@domain.com:pass@smtp.domain.com', // SMTP connection string
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+})
+export class MailModule {}
