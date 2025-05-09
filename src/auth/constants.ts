be0e1365196d8ab1ffc892f8ae89f53ed38d36b7
LiuYiBest <1070692044@qq.com>
2025-05-09 12:45:12 +0800

    jwtConstants

---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+  // 这是一个示例密钥，实际应用中请使用更复杂的密钥
+  // 并将其存储在安全的地方，例如环境变量或密钥管理服务中
+  secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+};
