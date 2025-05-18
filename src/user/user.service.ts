6a3ba3dc2cf972fcad3cb4aabfd307998afdcb37
LiuYiBest <1070692044@qq.com>
2025-05-19 01:26:38 +0800

    bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -6,35 +6,79 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UserService {
   @InjectEntityManager()
   private manager: EntityManager;
+  
   @InjectRepository(User)
   private userRepository: Repository<User>;
 
-  create(createUserDto: CreateUserDto) {
-    this.manager.save(User, createUserDto);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.userRepository.findOne({
+      where: [
+        { username: createUserDto.username },
+        { email: createUserDto.email }
+      ]
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Username or email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword
+    });
+
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return this.manager.find(User);
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find({
+      select: ['id', 'username', 'email', 'isVerified', 'lastLoginAt']
+    });
   }
 
-  findOne(id: number) {
-    return this.manager.findOne(User, {
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({
       where: { id },
+      select: ['id', 'username', 'email', 'isVerified', 'lastLoginAt']
     });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    this.manager.save(User, {
-      id: id,
-      ...updateUserDto,
-    });
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.findOne(id);
+    
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
+
+    Object.assign(user, updateUserDto);
+    return this.userRepository.save(user);
   }
 
-  remove(id: number) {
-    this.manager.delete(User, id);
+  async remove(id: number): Promise<void> {
+    const result = await this.userRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    return this.userRepository.findOne({ where: { username } });
   }
 }
