ad7dbd221cfb3fd15d56443f6075a89a6e8d1f72
LiuYiBest <1070692044@qq.com>
2025-05-21 13:24:50 +0800

    schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,15 +1,29 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({ required: true, unique: true })
+  @IsString()
   username: string;
 
+  @Prop({ required: true })
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @Prop({ required: true, unique: true })
+  @IsEmail()
+  email: string;
+
+  @Prop({ default: false })
+  isVerified: boolean;
+
   @Prop()
-  porssword: string;
+  lastLoginAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
