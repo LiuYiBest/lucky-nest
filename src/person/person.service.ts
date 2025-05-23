fcc48d4b7bc53ff53a7644f208c380a5b5a2ddc2
LiuYiBest <1070692044@qq.com>
2025-05-23 23:40:16 +0800

    service

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,35 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 import { OtherService } from 'src/other/other.service';
-import { Inject } from '@nestjs/common';
+import { Person } from './entities/person.entity';
 
 @Injectable()
 export class PersonService {
-  create(createPersonDto: CreatePersonDto) {
-    return `createPersonDto: ${JSON.stringify(createPersonDto)}`;
+  constructor(
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+    private readonly otherService: OtherService,
+  ) {}
+
+  async create(createPersonDto: CreatePersonDto): Promise<Person> {
+    const person = this.personRepository.create(createPersonDto);
+    return await this.personRepository.save(person);
   }
 
-  findAll() {
-    return `This action returns all person`;
+  async findAll(): Promise<Person[]> {
+    return await this.personRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  async findOne(id: number): Promise<Person> {
+    const person = await this.personRepository.findOne({ where: { id } });
+    if (!person) {
+      throw new NotFoundException(`Person with ID ${id} not found`);
+    }
+    return person;
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  async update(id: number, updatePersonDto: UpdatePersonDto): Promise<Person> {
+    const person = await this.findOne(id);
+    Object.assign(person, updatePersonDto);
+    return await this.personRepository.save(person);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  async remove(id: number): Promise<void> {
+    const result = await this.personRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Person with ID ${id} not found`);
+    }
   }
 
-  @Inject(OtherService)
-  private readonly otherService: OtherService;
-
-  getHello() {
+  getHello(): string {
     return 'Hello World!' + this.otherService.getHello();
   }
 }
