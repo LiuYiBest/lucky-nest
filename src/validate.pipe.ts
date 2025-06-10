ef43f97ba79812f5d80ee5a418e50548c4e84132
LiuYiBest <1070692044@qq.com>
2025-06-10 21:20:50 +0800

    validate

--- src/validate.pipe.ts
+++ src/validate.pipe.ts
@@ -3,10 +3,15 @@ import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from
 @Injectable()
 export class ValidatePipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    if (Number.isNaN(parseInt(value))) {
-      throw new BadRequestException(`参数${metadata.data}错误`);
+    if (value === undefined || value === null) {
+      throw new BadRequestException(`参数${metadata.data}不能为空`);
     }
 
-    return typeof value === 'number' ? value * 10 : parseInt(value) * 10;
+    const parsedValue = parseInt(value);
+    if (Number.isNaN(parsedValue)) {
+      throw new BadRequestException(`参数${metadata.data}必须是数字`);
+    }
+
+    return typeof value === 'number' ? value * 10 : parsedValue * 10;
   }
 }
